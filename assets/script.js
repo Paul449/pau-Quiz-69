f6c388a144ef15ac4e2301e3ae45e4c3f8bcc1a9
paul449 <paul_bilbatua@hotmail.com>
2024-02-16 11:51:31 -0600

    new changes

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,29 @@
 // This document will contain the logic and functionality for this quiz and students will be able to take it.
 
-// global variables
+                                                                    // global variables
+
+// divheader elements
+
+var getElementbyTimer = document.getElementById('timer'); // selecting HTML element from divheader called "timer" to convert it to a functional timer
+
+var getElementbyHighScores = document.getElementById('Highscores'); // selecting HTML element from divheader called "Highscores" to see the highest score after finishing quiz
+
+
+// Question options
 
 
 
 
+//button elements
 
+var getElementbybuttons = document.getElementsByTagName('button');
 
+var getElementbyStartQuiz = getElementbybuttons[0];
+
+var getElementbySubmitQuiz = getElementbybuttons[1];
+
+var getElementbyGoBack = getElementbybuttons[2];
+
+var getElementbyClearHighScores = getElementbybuttons[3];
 
 //functions
